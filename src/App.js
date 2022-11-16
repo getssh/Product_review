8c514fb4a92d6d740c7e679f73a24b01f0326514
getssh <gtamrat33@gmail.com>
2022-11-16 13:09:01 +0300

    colors and style added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
 import Product1 from './images/image-product-desktop.jpg'
+import Product2 from './images/image-product-mobile.jpg'
 
 function App() {
   return (
-    <div className="App container p-0 bg-white mt-5 col-md-6">
+    <div className="App container p-0 bg-white mt-5 col-md-6 br-5" style={{borderRadius: "1.5rem"}}>
         <div className='row m-0 p-0 d-flex'>
           <div className='col-md-6 col-sm-12 m-0 p-0'>
-            <img src={Product1}/>
+            <picture>
+            <source media='(min-width:650px)' srcSet={Product2}/>
+              <img src={Product1}/>
+            </picture>
           </div>
-          <div className='col-md-6 col-sm-12 ps-3'>
-            <p>Prefume</p>
-            <h2>Gabrielle Essence Eau De Parfum</h2>
-            <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+          <div className='col-md-6 col-sm-12 ps-3 mt-3'>
+            <p style={{color: "hsl(158, 36%, 37%)", fontSize: "1rem"}}>P E R F U M E</p>
+            <h1 style={{fontFamily:"Montserrat", fontWeight:"700"}}>Gabrielle Essence Eau De Parfum</h1>
+            <p style={{color: "hsl(158, 36%, 37%)"}}>A floral, solar and voluptuous interpretation composed by Olivier Polge, 
   Perfumer-Creator for the House of CHANEL.</p>
-            <h2>$149.99</h2>
-            <span><del>$169.99</del></span>
+            <div className='row'>
+              <br/>
+              <h2 style={{color: "hsl(158, 36%, 37%)"}} className='col-6'>$149.99</h2>
+              <span className='col-6 mt-2'><del>$169.99</del></span>
+            </div>
+            <button className='btn text-white col-9 mt-4' style={{backgroundColor: "hsl(158, 36%, 37%)", fontWeight: "bold"}}>
+              <svg className='me-2' width="15" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M14.383 10.388a2.397 2.397 0 0 0-1.518-2.222l1.494-5.593a.8.8 0 0 0-.144-.695.8.8 0 0 0-.631-.28H2.637L2.373.591A.8.8 0 0 0 1.598 0H0v1.598h.983l1.982 7.4a.8.8 0 0 0 .799.59h8.222a.8.8 0 0 1 0 1.599H1.598a.8.8 0 1 0 0 1.598h.943a2.397 2.397 0 1 0 4.507 0h1.885a2.397 2.397 0 1 0 4.331-.376 2.397 2.397 0 0 0 1.12-2.021ZM11.26 7.99H4.395L3.068 3.196h9.477L11.26 7.991Zm-6.465 6.392a.8.8 0 1 1 0-1.598.8.8 0 0 1 0 1.598Zm6.393 0a.8.8 0 1 1 0-1.598.8.8 0 0 1 0 1.598Z" fill="#FFF"/></svg>
+              Add to Cart
+            </button>
           </div>
           
         </div>
